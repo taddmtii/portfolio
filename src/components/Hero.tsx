3bb2e7a27bcc42241d2c0471df84c7a5afed1d3d
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-17 18:11:12 -0500

    added Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+export function Hero() {
+    return (
+        <div className="flex flex-col gap-4 w-lg h-lg">
+            <div>
+                <h1 className="font-bold text-6xl">Tadd Trumbull</h1>
+            </div>
+            <div>
+                <p className="text-xl opacity-40">Aspiring Software Engineer</p>
+            </div>
+            <div>
+                <p className="text-xl opacity-40">
+                    Building all sorts of things with many modern technologies.
+                    passionate about continuous learning and great user
+                    experience.
+                </p>
+            </div>
+            <div className="flex flex-row gap-2">
+                <div className="badge badge-lg badge-accent">Python</div>
+                <div className="badge badge-lg badge-accent">C++</div>
+                <div className="badge badge-lg badge-accent">React</div>
+                <div className="badge badge-lg badge-accent">TypeScript</div>
+                <div className="badge badge-lg badge-accent">C#</div>
+            </div>
+        </div>
+    );
+}
