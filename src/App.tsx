69dbd2ced279d51538178ef49838cc774d6aa904
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-17 19:45:56 -0500

    changed app structure, modified ExperienceCard component

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,48 @@ import { Divider } from "./components/Divider";
 import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import { TerminalCard } from "./components/TerminalCard";
+import ExperienceCard from "./components/ExperienceCard";
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Header />
-                <div className="flex flex-row">
-                    <Hero />
-                    <TerminalCard />
+                <div className="flex w-screen justify-center">
+                    <div className="flex flex-col w-[80vw] items-center">
+                        <div className="flex flex-row">
+                            <Hero />
+                            <TerminalCard />
+                        </div>
+                        <Divider />
+                        <AboutMe />
+                        <Divider />
+                        <Experience />
+                        <div className="flex flex-col gap-8 justify-center">
+                            <ExperienceCard
+                                dateRange="March 2025 - Present"
+                                positionName="Scholar Intern"
+                                company="Unum"
+                                description="Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "
+                                technologies={["Python", "SQL", "GraphQL"]}
+                            />
+                            <ExperienceCard
+                                dateRange="June 2024 - August 2024"
+                                positionName="Software Engineer Intern"
+                                company="Southern Adventist University"
+                                description="Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum "
+                                technologies={[
+                                    "C#",
+                                    "ASP.NET",
+                                    "SQL",
+                                    "HTML",
+                                    "CSS",
+                                ]}
+                            />
+                        </div>
+                        <Divider />
+                    </div>
                 </div>
-
-                <Divider />
-                <AboutMe />
-                <Divider />
-                <Experience />
-                <Divider />
             </BrowserRouter>
         </>
     );
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,7 +1,40 @@
-export default function ExperienceCard() {
+interface ExperienceCardProps {
+    dateRange: string;
+    positionName: string;
+    company: string;
+    description: string;
+    technologies: string[];
+}
+
+export default function ExperienceCard({
+    dateRange,
+    positionName,
+    company,
+    description,
+    technologies,
+}: ExperienceCardProps) {
     return (
         <>
-            <div></div>
+            <div className="flex flex-row justify-center bg-[#252839] rounded-2xl w-[80%]">
+                <div>{dateRange}</div>
+                <div className="flex flex-col">
+                    <div>{positionName}</div>
+                    <div>{company}</div>
+                    <div>{description}</div>
+                    <div className="flex flex-row gap-2">
+                        {technologies.map((tech) => {
+                            return (
+                                <div
+                                    key={tech}
+                                    className="badge badge-lg badge-accent"
+                                >
+                                    {tech}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
