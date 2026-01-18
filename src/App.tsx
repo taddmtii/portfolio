9fa05379771a90af1cf52b6d7112aeb2e77612d3
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-18 15:22:20 -0500

    removed header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import Header from "./components/Header";
-import { BrowserRouter } from "react-router";
 import { Hero } from "./components/Hero";
 import { Divider } from "./components/Divider";
 import AboutMe from "./components/AboutMe";
@@ -10,24 +8,19 @@ import { TerminalCard } from "./components/TerminalCard";
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <div className="sticky top-0 left-0 w-full">
-                    <Header />
-                </div>
-                <div className="flex w-screen items-center justify-center">
-                    <div className="flex flex-col w-full max-w-6xl mx-auto px-6 py-20">
-                        <div className="flex flex-col md:flex-row gap-12 items-start">
-                            <Hero />
-                            <TerminalCard />
-                        </div>
-                        <Divider />
-                        <AboutMe />
-                        <Divider />
-                        <Experience />
-                        <Divider />
+            <div className="flex w-screen items-center justify-center">
+                <div className="flex flex-col w-full max-w-6xl mx-auto px-6 py-20">
+                    <div className="flex flex-col md:flex-row gap-12 items-start">
+                        <Hero />
+                        <TerminalCard />
                     </div>
+                    <Divider />
+                    <AboutMe />
+                    <Divider />
+                    <Experience />
+                    <Divider />
                 </div>
-            </BrowserRouter>
+            </div>
         </>
     );
 }
