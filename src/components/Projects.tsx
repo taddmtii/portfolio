afbb4fd312117e2f0401adea5c77aa9075a28877
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-29 12:53:08 -0500

    updated terminalcard status

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,40 +1,40 @@
 import ProjectCard from "./ProjectCard";
 
 export function Projects() {
-    return (
-        <>
-            <div className="flex flex-col gap-8">
-                <h1 className="font-bold text-3xl">Projects</h1>
-                <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
-                    <ProjectCard
-                        name="ArrayViz"
-                        thumbnail="https://github.com/user-attachments/assets/fdf39753-8bc7-40fb-8337-0016d53ad0bc"
-                        link="https://github.com/taddmtii/ArrayViz"
-                        description="Visualization of array operations and sorting algorithms utilizing custom made language. Created using React and Typescript."
-                    />
-                    <ProjectCard
-                        name="Samurai Sprint"
-                        thumbnail="https://github.com/user-attachments/assets/b3432a23-c114-4fe9-93f1-9150c17c1724"
-                        link="https://github.com/taddmtii/Samurai-Sprint"
-                        description="A 2D platformer game made with Godot Engine and GDScript, a scripting language similar to Python and Typescript."
-                    />
-                </div>
-                <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
-                    <ProjectCard
-                        name="FlowFinder"
-                        thumbnail="https://github.com/user-attachments/assets/2a6b7c57-8fd1-4d87-8a49-074d03516399"
-                        link="https://github.com/taddmtii/FlowFinder"
-                        description="Web application built with Python and Flask to provide users with insights into thier Spotify usage and statistics."
-                    />
+  return (
+    <>
+      <div className="flex flex-col gap-8">
+        <h1 className="font-bold text-3xl">Projects</h1>
+        <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
+          <ProjectCard
+            name="ArrayViz"
+            thumbnail="https://github.com/user-attachments/assets/fdf39753-8bc7-40fb-8337-0016d53ad0bc"
+            link="https://github.com/taddmtii/ArrayViz"
+            description="Visualization of array operations and sorting algorithms utilizing custom made language. Created using React and Typescript."
+          />
+          <ProjectCard
+            name="Samurai Sprint"
+            thumbnail="https://github.com/user-attachments/assets/b3432a23-c114-4fe9-93f1-9150c17c1724"
+            link="https://github.com/taddmtii/Samurai-Sprint"
+            description="A 2D platformer game made with Godot Engine and GDScript, a scripting language similar to Python and Typescript."
+          />
+        </div>
+        <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
+          <ProjectCard
+            name="FlowFinder"
+            thumbnail="https://github.com/user-attachments/assets/2a6b7c57-8fd1-4d87-8a49-074d03516399"
+            link="https://github.com/taddmtii/FlowFinder"
+            description="Web application built with Python and Flask to provide users with insights into thier Spotify usage and statistics."
+          />
 
-                    <ProjectCard
-                        name="CareerConnector"
-                        thumbnail="https://github.com/taddmtii/CareerConnector/assets/122894305/3d68e774-d821-4ad7-b016-815d2f69b18e"
-                        link="https://github.com/taddmtii/CareerConnector"
-                        description="Web application built with ASP.NET Razor Pages, C# and SQlite."
-                    />
-                </div>
-            </div>
-        </>
-    );
+          <ProjectCard
+            name="CareerConnector"
+            thumbnail="https://github.com/taddmtii/CareerConnector/assets/122894305/3d68e774-d821-4ad7-b016-815d2f69b18e"
+            link="https://github.com/taddmtii/CareerConnector"
+            description="Web application built with ASP.NET Razor Pages, C# and SQlite."
+          />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/TerminalCard.tsx
+++ src/components/TerminalCard.tsx
@@ -1,41 +1,41 @@
 export function TerminalCard() {
-    return (
-        <>
-            <div className="mockup-code bg-primary text-primary-content w-full h-85 max-w-xl">
-                <pre data-prefix="$">
-                    <code>name</code>
-                </pre>
-                <pre data-prefix=">" className="text-success">
-                    <code>Tadd Trumbull II</code>
-                </pre>
-                <pre data-prefix="$">
-                    <code>pwd</code>
-                </pre>
-                <pre data-prefix=">" className="text-success">
-                    <code>chattanooga, tn</code>
-                </pre>
-                <pre data-prefix="$">
-                    <code>cat contact.json</code>
-                </pre>
-                <pre data-prefix=">" className="text-success">
-                    <code>taddtrumbullii@gmail.com</code>
-                </pre>
-                <pre data-prefix="$">
-                    <code>cat coffee.log</code>
-                </pre>
-                <pre data-prefix=">" className="text-success">
-                    <code>cups of coffee today: 3</code>
-                </pre>
-                <pre data-prefix="$">
-                    <code>status</code>
-                </pre>
-                <pre data-prefix=">" className="text-success">
-                    <code>building: something cool</code>
-                </pre>
-                <pre data-prefix=">" className="text-success">
-                    <code>learning: new web technologies</code>
-                </pre>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="mockup-code bg-primary text-primary-content w-full h-85 max-w-xl">
+        <pre data-prefix="$">
+          <code>name</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>Tadd Trumbull II</code>
+        </pre>
+        <pre data-prefix="$">
+          <code>pwd</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>chattanooga, tn</code>
+        </pre>
+        <pre data-prefix="$">
+          <code>cat contact.json</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>taddtrumbullii@gmail.com</code>
+        </pre>
+        <pre data-prefix="$">
+          <code>cat coffee.log</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>cups of coffee today: 3</code>
+        </pre>
+        <pre data-prefix="$">
+          <code>status</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>building: <a href="https://github.com/taddmtii/Cadence" target="_blank">Cadence</a></code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>learning: NestJS + Prisma + Postgres</code>
+        </pre>
+      </div>
+    </>
+  );
 }
