2f2e5ffe448dc6fda741adc6574403a3cde49cae
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-06-08 19:25:50 -0400

    changed experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,32 +1,32 @@
 import ExperienceCard from "./ExperienceCard";
 
 export default function Experience() {
-    return (
-        <>
-            <div className="flex flex-col gap-8">
-                <h1 className="font-bold text-3xl">Experience</h1>
-                <ExperienceCard 
-                    dateRange="June 2026"
-                    positionName="Associate Software Engineer" 
-                    description="..."
-                    company="Unum"
-                    technologies={["C#", ".NET"]}
-                />
-                <ExperienceCard
-                    dateRange="March 2025 - Present"
-                    positionName="Scholar Intern"
-                    description="Maintain and optimize financial platforms while developing automation solutions."
-                    company="Unum"
-                    technologies={["Python", "SQL", "GraphQL"]}
-                />
-                <ExperienceCard
-                    dateRange="June 2024 - August 2024"
-                    positionName="Software Engineer Intern"
-                    description="Modernized internal academic platform by refactoring legacy tool."
-                    company="Southern Adventist University"
-                    technologies={["C#", ".NET", "SQL"]}
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col gap-8">
+        <h1 className="font-bold text-3xl">Experience</h1>
+        <ExperienceCard
+          dateRange="June 2026 - Present"
+          positionName="Associate Software Engineer"
+          description="..."
+          company="Unum"
+          technologies={["C#", ".NET"]}
+        />
+        <ExperienceCard
+          dateRange="March 2025 - June 2026"
+          positionName="Scholar Intern"
+          description="Maintain and optimize financial platforms while developing automation solutions."
+          company="Unum"
+          technologies={["Python", "SQL", "GraphQL"]}
+        />
+        <ExperienceCard
+          dateRange="June 2024 - August 2024"
+          positionName="Software Engineer Intern"
+          description="Modernized internal academic platform by refactoring legacy tool."
+          company="Southern Adventist University"
+          technologies={["C#", ".NET", "SQL"]}
+        />
+      </div>
+    </>
+  );
 }
