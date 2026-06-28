5215054402560fa840f48f824d1b8ef247736840
Tadd Trumbull <taddtrumbullii@gmail.com>
2026-06-27 22:33:22 -0400

    fixed issue with unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Hero } from "./components/Hero";
 import Experience from "./components/Experience";
-import { TerminalCard } from "./components/TerminalCard";
 import { Projects } from "./components/Projects";
 import Contact from "./components/Contact";
 
@@ -12,7 +11,6 @@ function App() {
         <div className="flex flex-col w-full max-w-6xl mx-auto px-8 py-20">
           <div className="flex flex-col md:flex-row gap-12 justify-center items-center md:items-start">
             <Hero />
-            {/*<TerminalCard />*/}
           </div>
           <div className="divider my-10"></div>
           <Experience />
