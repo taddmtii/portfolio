7b2f6ff799eebddee0d7ff9d655599b178aa3a59
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-08-02 10:16:39 -0400

    added project and rearranged cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,6 +7,13 @@ export function Projects() {
         <h1 className="font-bold text-3xl">Projects</h1>
         <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
           <ProjectCard
+            name="Soundprint"
+            link="https://github.com/taddmtii/Soundprint"
+            description="A live insight dashboard for your Spotify listening stats."
+            technologies={["React", "TypeScript", "PostgreSQL"]}
+            inProgress={true}
+          />
+          <ProjectCard
             name="Code Visualizer"
             link="https://github.com/taddmtii/CodeVisualizer"
             description="
@@ -14,7 +21,9 @@ export function Projects() {
             technologies={["React", "TypeScript", "Nearley"]}
             inProgress={false}
           />
-          <ProjectCard
+        </div>
+        <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
+                    <ProjectCard
             name="Cadence"
             link="https://github.com/taddmtii/Cadence"
             description="
@@ -22,8 +31,6 @@ export function Projects() {
             technologies={["React", "TypeScript", "PostgreSQL"]}
             inProgress={false}
           />
-        </div>
-        <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
           <ProjectCard
             name="Nuclear Simulator"
             link="https://github.com/taddmtii/NuclearSimulator"
@@ -31,20 +38,13 @@ export function Projects() {
             technologies={["React", "TypeScript"]}
             inProgress={false}
           />
-          <ProjectCard
+        </div>
+        <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
+                    <ProjectCard
             name="Samurai Sprint"
             link="https://github.com/taddmtii/Samurai-Sprint"
             description="A 2D platformer passion project where you play as a Samurai."
             technologies={["Godot", "GDScript"]}
-            inProgress={false}
-          />
-        </div>
-        <div className="flex flex-col items-center md:flex-row gap-8 md:justify-evenly">
-          <ProjectCard
-            name="FlowFinder"
-            link="https://github.com/taddmtii/FlowFinder"
-            description="Web application to provide users with insights into their Spotify usage and statistics."
-            technologies={["Python", "Flask", "HTML"]}
             inProgress={false}
           />
           <ProjectCard
